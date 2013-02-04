2644bc43ab5b5ec7c6f8a228c227d460069e506b
Katie Smillie <katiesmillie@gmail.com>
2013-02-04 14:59:57 -0800

    user_id is an accessible field in the answer model

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,6 +2,6 @@ class Answer < ActiveRecord::Base
   validates_presence_of :body, :question_id, :user_id
   belongs_to :question
   belongs_to :user
-  attr_accessible :body, :question_id
+  attr_accessible :body, :question_id, :user_id
   
 end
