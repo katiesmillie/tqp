6c9a70709ae638fb6531aa77cfcff2a41e01ed59
Katie Smillie <katiesmillie@gmail.com>
2013-02-07 15:43:06 -0800

    added edit, update and destroy to answer controller

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -3,7 +3,8 @@ class AnswersController < ApplicationController
   before_filter :require_user, :except => :new
   
   def new
-    @question=Question.scoped.sample
+    @question=UserQuestion.scoped.sample
+    @user_answers=@question.answers.where(:user_id => current_user.id)
   end
   
   def create
@@ -11,12 +12,30 @@ class AnswersController < ApplicationController
     @answer.user = current_user
     @answer.save
     redirect_to answers_path
-    
   end
   
   def index
     @answers=current_user.answers.order "created_at DESC"
   end
+  
+  def edit
+    @answer=Answer.find params[:id]
+    @question=@answer.question
+  end
+  
+  def update
+    @answer=Answer.find params[:id]
+    @answer.body = params[:body]
+    @answer.save
+    redirect_to answers_path
+  end
+  
+  def destroy
+    @answer=Answer.find params[:id]
+    @answer.delete
+    redirect_to answers_path
+  end
+
   
 end
   
