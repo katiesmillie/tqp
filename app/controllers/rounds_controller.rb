b221eca479a7b637ce6aaf1868893eaf22fa2b59
Katie Smillie <katiesmillie@gmail.com>
2013-02-17 16:14:09 -0800

    created round#index and require user to be logged in

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -1,5 +1,7 @@
 class RoundsController < ApplicationController
+  
   before_filter :require_answer, :only => [:show]
+  before_filter :require_user
   
 #each round is a question per day for a pair, each round has a page
   def show
@@ -8,6 +10,23 @@ class RoundsController < ApplicationController
     @answers=@round.answers
   end
   
+  def index
+    @pair=Pair.where("user1_id = ? OR user2_id = ?", current_user.id, current_user.id)
+    @rounds=Round.order "round_date DESC"
+      
+      @rounds.each do |r|
+        if r.answers.count > 0
+          @answer_label = "View Round"
+        else
+          @answer_label = "Answer Question" 
+        end
+      end
+      
+  end
+  
+  def require_user
+    redirect_to welcome_path unless current_user 
+  end
 
   protected 
   def require_answer
