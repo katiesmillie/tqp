a1f9ddac46faa146c4e0ca6c96c6af137354dac1
Katie Smillie <katiesmillie@gmail.com>
2013-02-21 21:49:50 -0800

    answers belong to rounds, comments below to authors

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,6 +2,7 @@ class Answer < ActiveRecord::Base
   validates_presence_of :body, :question_id, :user_id, :round_id
   belongs_to :question
   belongs_to :user
+  belongs_to :round
   attr_accessible :body, :question_id, :user_id, :round_id
   
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,6 @@
 class Comment < ActiveRecord::Base
   validates_presence_of :body, :author_id, :round_id
   belongs_to :round
+  belongs_to :author, :class_name => "User" 
   attr_accessible :body, :author_id, :round_id
 end
