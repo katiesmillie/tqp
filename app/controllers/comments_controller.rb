0878647d29ff3684c4e1e1f791a2f0a7b9bb4b6c
Katie Smillie <katiesmillie@gmail.com>
2013-02-21 21:52:26 -0800

    Adjustments to comments, added editing of comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,21 @@
 class CommentsController < ApplicationController
   
   def create
-        @comment=Comment.new :body => params[:body],:author_id => current_user.id, :round_id => params[:round_id]
-        @comment.save
-        
-        redirect_to round_path(:id => params[:round_id])
+    @comment=Comment.new :body => params[:body],:author_id => current_user.id, :round_id => params[:round_id]
+    @comment.save   
+    redirect_to round_path(:id => params[:round_id])
   end
+  
+  def edit
+    @comment=Comment.find params[:id]
+  end
+  
+  def update
+    @comment=Comment.find params[:id]
+    @comment.body = params[:body]
+    @comment.save
+    redirect_to round_path
+  end
+  
   
 end
