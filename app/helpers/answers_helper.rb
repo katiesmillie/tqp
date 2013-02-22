8ff1dab257f854a3cccf46ae5d4914d5674d0f6d
Katie Smillie <katiesmillie@gmail.com>
2013-02-21 21:41:01 -0800

    created pair_status in helper for answer#new

--- app/helpers/answers_helper.rb
+++ app/helpers/answers_helper.rb
@@ -1,2 +1,15 @@
 module AnswersHelper
+  
+  def pair_status(round)
+    @partner_name = round.pair.partner(current_user.id).first_name
+    
+      if round.answers.count < 1
+        "#{@partner_name.capitalize} has not answered"
+       else
+         "Submit your reponse to see #{@partner_name}'s answer"
+      end
+    
+  end
+    
+          
 end
