563ed273b704bb43fefb580685341932b1ce85fe
Katie Smillie <katiesmillie@gmail.com>
2013-02-25 15:07:12 -0800

    Pair is only created if both users don't already belong to a pair

--- app/controllers/pairs_controller.rb
+++ app/controllers/pairs_controller.rb
@@ -7,13 +7,14 @@ class PairsController < ApplicationController
   end
 
 
-  def create
-    @pair=Pair.new :user1_id => current_user.id, :user2_id => params[:partner_id]
-    
-    if current_user.pair.nil?
-      @pair.save
-    else
+  def create      
+      @partner=User.find params[:partner_id]
+
+    if current_user.pair.nil? && @partner.pair.nil?
+      @pair=Pair.create :user1_id => current_user.id, :user2_id => params[:partner_id]
       redirect_to pair_path(current_user.id)
+    else
+      redirect_to new_pair_path
     end
     
   end
