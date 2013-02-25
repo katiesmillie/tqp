93754809bf21f7954751f09a4ed3c6e1afbd9cc7
Katie Smillie <katiesmillie@gmail.com>
2013-02-25 10:14:57 -0800

    updated daily question email

--- app/mailers/questions_mailer.rb
+++ app/mailers/questions_mailer.rb
@@ -1,7 +1,16 @@
 class QuestionsMailer < ActionMailer::Base
   default :from => "admin@thequestionproject.com"
-  
+   
   def daily_question(user, question)
+     @user = user
+     @question = @round.question
+     @url = "http://thequestionproject.com"
+     mail(:to => user.email, :subject => "Your Daily Question")
+    
+
+     
+  end
+  
     @user = user
     @question = question
     mail(:to => user.email, :subject => "Your Daily Question")
