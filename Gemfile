6fc73c6bb338bb45190e87afa1eb0cc667573f05
Katie Smillie <katiesmillie@gmail.com>
2013-03-07 15:15:51 -0800

    added air brake

--- Gemfile
+++ Gemfile
@@ -51,3 +51,4 @@ gem "devise"
 
 gem "letter_opener", :group => :development
 
+gem "airbrake"
---
+++ config/initializers/airbrake.rb
@@ -0,0 +1,3 @@
+Airbrake.configure do |config|
+  config.api_key = '9125508c08e01032a54786290f14f53f'
+end
