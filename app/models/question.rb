0d1fe8aa5b59ad7c20c286b332058bba5aa0a1be
Katie Smillie <katiesmillie@gmail.com>
2013-03-06 18:06:34 -0800

    author is part of user class

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,5 +2,6 @@ class Question < ActiveRecord::Base
   validates_presence_of :body
   has_many :answers
   has_many :rounds
+  belongs_to :author, :class_name => "User"
   attr_accessible :body, :category, :author_id
 end
