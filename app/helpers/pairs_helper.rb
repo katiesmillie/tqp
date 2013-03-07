7a40611bcfdb0fb947ae9a8909fc550b4594e5dc
Katie Smillie <katiesmillie@gmail.com>
2013-03-07 14:47:36 -0800

    Hid search result box unless search results, added icons

--- app/helpers/pairs_helper.rb
+++ app/helpers/pairs_helper.rb
@@ -4,6 +4,12 @@ module PairsHelper
     @partnered = "That person already has a partner"
   end
   
+  def search_result(partners)
+    if partners.count > 0
+      yield
+    end
+  end
+  
   
   
 end
