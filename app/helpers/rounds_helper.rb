c1e7a9e2a6cfedf3c2b8a91488455fa868726109
Katie Smillie <katiesmillie@gmail.com>
2013-03-06 18:04:42 -0800

    detects presence of comments for rounds, filters edit so user can only edit comments and answers that belong to them. plus styling changes in round views

--- app/helpers/rounds_helper.rb
+++ app/helpers/rounds_helper.rb
@@ -8,6 +8,27 @@ module RoundsHelper
       end
   end
   
+  def comment_present(round)
+    if round.comments.count > 0
+      yield
+    end
+  end
+  
+  
+  def comment_edit(comment)
+    if comment.author == current_user
+      yield
+    end
+  end
+  
+  
+  def answer_edit(answer)
+    if answer.user == current_user
+      yield
+    end
+  end
+         
+  
 
   
 end
