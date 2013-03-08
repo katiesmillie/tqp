9bacdec19ecc911ca6be0de86752dee1aedca5f5
Katie Smillie <katiesmillie@gmail.com>
2013-03-08 00:50:36 -0800

    round is created when a pair is first created

--- app/controllers/pairs_controller.rb
+++ app/controllers/pairs_controller.rb
@@ -7,12 +7,14 @@ class PairsController < ApplicationController
   end
 
 
-  def create      
+  def create    
       @partner=User.find params[:partner_id]
 
     if current_user.pair.nil? && @partner.pair.nil?
       @pair=Pair.create :user1_id => current_user.id, :user2_id => params[:partner_id]
-      redirect_to user_path(current_user.id)
+      @question=Question.scoped.sample
+      @round=Round.create :question_id => @question.id, :pair_id => @pair.id, :round_date => Time.now.midnight
+      redirect_to root_path
     else
       redirect_to new_pair_path
     end
