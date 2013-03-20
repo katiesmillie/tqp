5ae46a30af14d4f5c191eac072d19e332b502468
Katie Smillie <katiesmillie@gmail.com>
2013-03-20 14:17:27 -0700

    remove migration

--- db/migrate/20130226023153_remove_round_id_from_rounds.rb
+++
@@ -1,9 +0,0 @@
-class RemoveRoundIdFromRounds < ActiveRecord::Migration
-  def up
-    remove_column :rounds, :round_id
-      end
-
-  def down
-    add_column :rounds, :round_id, :date
-  end
-end
