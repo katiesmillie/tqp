cba2fea30d555172460717d7200e94710c579de3
Katie Smillie <katiesmillie@gmail.com>
2013-03-21 13:43:19 -0700

    fixed more errors

--- app/controllers/incoming_mails_controller.rb
+++ app/controllers/incoming_mails_controller.rb
@@ -3,10 +3,6 @@ class IncomingMailsController < ApplicationController
   skip_before_filter :verify_authenticity_token
   
    def create
-      # Rails.logger.info params[:headers][:subject]
-      # Rails.logger.info params[:envelope][:from]
-      # Rails.logger.info params[:plain]
-      # Rails.logger.info params[:html]    
 
       @subject=params[:headers][:Subject]    
       @round=Round.where(:id => @subject[/\d+/]).first
@@ -19,17 +15,13 @@ class IncomingMailsController < ApplicationController
       
       @user=@answer.user
       @pair=@user.pair
-      
-      #if round belongs to the user pair then save
-      
-      if @round.pair == @pair && @round.question.answer.nil?
-        @answer.save 
+    
+      if @answer.save 
         render :text => 'Answer saved', :status => 200
       else
         render :text => 'Answer not saved', :status => 400
       end
      
-    
     end
 
 end
