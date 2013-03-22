eb000d735ed3eca894658e69fa2ed397ae055018
Katie Smillie <katiesmillie@gmail.com>
2013-03-22 12:51:10 -0700

    Fixed no results message for searching a partner

--- app/controllers/pairs_controller.rb
+++ app/controllers/pairs_controller.rb
@@ -3,12 +3,19 @@ class PairsController < ApplicationController
 # connecting two users together as a pair, this happens on registration
 
   def new
-    @partners=User.where :email => params[:email]
+    @partners=User.where(:email => params[:email])
+    
+    if User.where(:email => params[:email]).count == 0
+      @label = "No results found. Try again or invite your friend to join!"
+    else
+      @label = ""
+    end
+    
   end
 
 
   def create    
-      @partner=User.find params[:partner_id]
+    @partner=User.find params[:partner_id]
 
     if current_user.pair.nil? && @partner.pair.nil?
       @pair=Pair.create :user1_id => current_user.id, :user2_id => params[:partner_id]
@@ -16,6 +23,7 @@ class PairsController < ApplicationController
       @round=Round.create :question_id => @question.id, :pair_id => @pair.id, :round_date => Time.now.midnight
       redirect_to root_path
     else
+      flash[:notice]="User already has a partner!"
       redirect_to new_pair_path
     end
     
