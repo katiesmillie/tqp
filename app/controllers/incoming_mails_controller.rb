cc47fe18cd7d5135b76ffa51210e59201b4d8656
Katie Smillie <katiesmillie@gmail.com>
2013-03-25 21:01:47 -0700

    added email notification when answering via email reply

--- app/controllers/incoming_mails_controller.rb
+++ app/controllers/incoming_mails_controller.rb
@@ -17,6 +17,7 @@ class IncomingMailsController < ApplicationController
       @pair=@user.pair
     
       if @answer.save 
+        User.mail_answer(current_user, @round)
         render :text => 'Answer saved', :status => 200
       else
         render :text => 'Answer not saved', :status => 400
