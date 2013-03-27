9447dc69c32166e335139833c8fee4a57cf40a5a
Katie Smillie <katiesmillie@gmail.com>
2013-03-27 13:47:42 -0700

    cleaning up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,10 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
+
+ protect_from_forgery
   
   def require_user
     redirect_to new_user_session_path unless current_user 
   end
-  
-  
-
-  
-  
   
   
   
