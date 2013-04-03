a718187cf4c0e784c1ad9c5ad332a07ac5ba069b
Katie Smillie <katiesmillie@gmail.com>
2013-04-03 08:46:10 -0700

    change flash to custom message, redirect back to same page after inviting

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -11,19 +11,21 @@ class InvitesController < ApplicationController
     @invite=Invite.new :email => params[:email], :message => params[:body]
     @invite.user=current_user
     
-    
     if params[:kind] == "partner"
       @invite.kind="partner"
       @invite.save 
       Invite.mail_invite(current_user, @invite.email, @invite.message)
+      flash[:success]="Your invitation was sent!"
+      redirect_to new_pair_path 
     elsif params[:kind] == "share"
       @invite.kind="share"
       @invite.save 
       Invite.mail_share(current_user, @invite.email, @invite.message)
+      flash[:success]="Your message was sent!"
+      redirect_to new_invite_path 
     end
-    
-
-    redirect_to new_pair_path 
+      
+   
   end
   
   
