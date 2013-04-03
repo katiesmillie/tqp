c459711852e0909bbb11f1226e44ac22d231dca5
Katie Smillie <katiesmillie@gmail.com>
2013-04-03 08:51:37 -0700

    refactoring and added method for mail_accepted

--- app/models/invite.rb
+++ app/models/invite.rb
@@ -8,12 +8,11 @@ class Invite < ActiveRecord::Base
     @user=user
     @email=email
     @message=message
-    
-    if message.to_s == ''
-      @display_message= ''
-    else
-      @display_message="#{@user.first_name} left you a message:"
-    end
+
+     if message.to_s.empty?
+     else
+       @display_message="#{@user.first_name} left you a message:"
+     end
     
     @url="http://beta.thequestionproject.com"
    
@@ -26,17 +25,33 @@ class Invite < ActiveRecord::Base
     @email=email
     @message=message
     
-    if message.to_s == ''
-      @display_message= ''
-    else
-      @display_message="#{@user.first_name} left you a message:"
-    end
-    
+      if message.to_s.empty?
+      else
+        @display_message="#{@user.first_name} left you a message:"
+      end
+
     @url="http://beta.thequestionproject.com"
-   
     InvitesMailer.invite_share(@user,@email,@message,@display_message, @url).deliver
     
   end
+  
+  def mail_accepted(user, partner, pair)
+     @pair=pair
+     @user=user
+     @partner=partner
+     
+     if @pair
+       @round=Round.where(:pair_id => @pair.id).first
+       @question=@round.question.body
+       @url="http://beta.thequestionproject.com/rounds/#{@round.id}"
+       
+       InvitesMailer.invite_accepted(@user,@partner,@pair,@round,@url,@question).deliver
+    end
+   end  
+  
+  
+
+  
     
   
 end
