0e927248da19187815663712303adc9f8f550dd3
Katie Smillie <katiesmillie@gmail.com>
2013-04-03 19:17:35 -0700

    added image to users model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130326043157) do
+ActiveRecord::Schema.define(:version => 20130403034537) do
 
   create_table "answers", :force => true do |t|
     t.string   "body"
@@ -92,6 +92,7 @@ ActiveRecord::Schema.define(:version => 20130326043157) do
     t.datetime "updated_at",                             :null => false
     t.string   "first_name"
     t.string   "last_name"
+    t.string   "image"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
