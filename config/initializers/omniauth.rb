8a34fb739b621a15e7e945d23ee250322413c288
Katie Smillie <katiesmillie@gmail.com>
2013-04-20 20:44:58 -0700

    changed Facebook token to work in production

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,7 +4,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   #create linked in app
   provider :linked_in, '54lpcvck0g3c', '48fK1J3k9nw6Pp51'
   #development
-  provider :facebook, '314070718699030', '9aa5266e67b68242c6324bf96cb3b392' if Rails.env.development?
+  provider :facebook, '314070718699030', '9aa5266e67b68242c6324bf96cb3b392' if Rails.env.production?
 
 #create another facebook app for production  
   # provider :facebook, '314070718699030', '9aa5266e67b68242c6324bf96cb3b392' if Rails.env.production?
