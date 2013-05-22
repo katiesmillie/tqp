2b17af08d701dc5996fd66b4459080564d480701
Katie Smillie <katiesmillie@gmail.com>
2013-05-21 21:00:46 -0700

    how to change to secret token when ready

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,11 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
+
+# if Rails.env.production?
+#  TheQuestionProject::Application.config.secret_token = ENV['RAILS_SECRET_TOKEN']
+# else
+#    TheQuestionProject::Application.config.secret_token = "whatever"
+# end
+
 TheQuestionProject::Application.config.secret_token = '5bc6a5702fb8e2729ecadc0cf034a54369f3ffeb1bae2de3642d2934715029d4fef2fb568426e256953a1f2ca715f69e448d4aad788131ffa95c2bc97d7227a8'
