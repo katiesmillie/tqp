4d6f1cbec7b98f620354c817428bf0b3a7f130bc
Katie Smillie <katiesmillie@gmail.com>
2013-05-21 21:01:51 -0700

    example of where to put css styling for emails. use helpers!

---
+++ app/helpers/mailers_helper.rb
@@ -0,0 +1,18 @@
+module MailersHelper
+
+
+#   def link_to_in_color(text, url)
+#     link_to text, url, :style  => "color:#FAF5F7;"
+#   end
+# 
+#   def header_styling
+#     "background-color:#3604CC; padding:1.1em;font:small 'Roboto', Helvetica, Arial, sans-serif;font-size:1em;"
+#   end
+# 
+#   def content_styling
+#     "padding:1.1em; font:small 'Roboto', Helvetica, Arial, sans-serif;font-size:1em;"
+#   end
+# 
+# end
+
+# <div class="content" style="<%= content_styling %>">
