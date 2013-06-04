c0e7c4e10563d26a2eb1e6560348565c00e3afc9
Katie Smillie <katiesmillie@gmail.com>
2013-06-03 20:40:01 -0700

    remove create round after pair is created, no longer needed

--- app/models/pair.rb
+++ app/models/pair.rb
@@ -5,7 +5,7 @@ class Pair < ActiveRecord::Base
   has_many :rounds
   attr_accessible :user1_id, :user2_id
   
-  after_create :create_round
+  # after_create :create_round
   
   def partner(user_id)
     if user_id == self.user1_id  # checking equality, comparing ids is faster then checking the user
